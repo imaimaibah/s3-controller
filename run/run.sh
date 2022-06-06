e8a71b04278712950af1adf6bd9ae937497e7027
Shin Imai <shin.imai@sedex.com>
2022-06-06 10:11:17 +0100

    Fixed run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ ! -d $(pwd)/project ];then
-  mkdir -p $(pwd)/project
-fi
 CERT_AUTH_DATA=$(kubectl config view --raw -o json | jq -r '.clusters[] | select(.name=="kind-kind").cluster."certificate-authority-data"')
 USER_CERT_DATA=$(kubectl config view --raw -o json | jq -r '.users[] | select(.name=="kind-kind").user."client-certificate-data"')
 USER_KEY_DATA=$(kubectl config view --raw -o json | jq -r '.users[] | select(.name=="kind-kind").user."client-key-data"')
@@ -33,6 +30,6 @@ docker run --rm -it \
   -e AWS_ROLE_ARN=arn:aws:iam::158484697723:role/connect-dev-external-dns,AWS_WEB_IDENTITY_TOKEN_FILE=token,AWS_REGION=eu-west-2 \
   --workdir /go/src/github.com/imaimaibah/s3-controller \
   -v $(pwd)/.kubeconfig:/root/.kube/config \
-  -v $(pwd)/project:/go/src/github.com/imaimaibah/s3-controller \
+  -v $(pwd):/go/src/github.com/imaimaibah/s3-controller \
   kubebuilder:s3-controller
 
